a7bf7120de283f5ee62d9ed00c511d1f0b5908e3
August Luhrs <augustluhrs@gmail.com>
2021-09-25 18:08:27 -0400

    Day 27 -- Marble Madness

---
+++ unity/27_MarbleMadness/Assets/Scripts/NeuralNetwork.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+//based on The One's Youtube video "Tutorial on Programming an Evolving Neural Network in C# w/ Unity3D"
+//thanks so much!
+
+public class NeuralNetwork : IComparable<NeuralNetwork>
+{
+    private int[] layers; //layers of the NN
+    private float[][] neurons; //the neuron matrix
+    private float[][][] weights; //the weights matrix
+    private float fitness; //fitness of the NN
+    private float normalizedFitness;
+    //initialize NN with random weights
+    public NeuralNetwork(int[] layers)
+    {
+        //deep copy of the layers
+        this.layers = new int[layers.Length];
+        for (int i = 0; i < layers.Length; i++)
+        {
+            this.layers[i] = layers[i];
+        }
+
+        //generate the matrices
+        InitNeurons();
+        InitWeights();
+    }
+
+    //deep copy constructor
+    public NeuralNetwork(NeuralNetwork copyNetwork)
+    {
+        this.layers = new int[copyNetwork.layers.Length];
+        for (int i = 0; i < copyNetwork.layers.Length; i++)
+        {
+            this.layers[i] = copyNetwork.layers[i];
+        }
+
+        InitNeurons();
+        InitWeights();
+        CopyWeights(copyNetwork.weights);
+    }
+
+    //copies the weights, duh
+    private void CopyWeights(float[][][] copyWeights)
+    {
+        for (int i = 0; i < weights.Length; i++)
+        {
+            for (int j = 0; j < weights[i].Length; j++)
+            {
+                for (int k = 0; k < weights[i][j].Length; k++)
+                { 
+                    weights[i][j][k] = copyWeights[i][j][k];
+                }
+            }
+        }
+    }
+
+    //create the neuron matrix
+    private void InitNeurons()
+    {
+        //Initialize
+        List<float[]> neuronsList = new List<float[]>();
+
+        for (int i = 0; i < layers.Length; i++)
+        {
+            neuronsList.Add(new float[layers[i]]); //add each layer to the neuron list
+        }
+
+        neurons = neuronsList.ToArray();
+    }
+
+    //create the weights matrix
+    private void InitWeights()
+    {
+        List<float[][]> weightsList = new List<float[][]>();
+
+        //iterate over all neurons that have weights connected to previous layer
+        for (int i = 1; i < layers.Length; i++)
+        {
+            List<float[]> layerWeightsList = new List<float[]>();
+            int neuronsInPreviousLayer = layers[i-1];
+
+            //iterate over all neurons in this layer
+            for (int j = 0; j < neurons[i].Length; j++)
+            {
+                float[] neuronWeights = new float[neuronsInPreviousLayer];
+                
+                //iterate over all neurons in the previous layer and set random weights
+                for (int k = 0; k < neuronsInPreviousLayer; k++)
+                {
+                    neuronWeights[k] = UnityEngine.Random.Range(-0.5f, 0.5f);
+                }
+
+                layerWeightsList.Add(neuronWeights);
+            }
+
+
+            weightsList.Add(layerWeightsList.ToArray());
+        }
+
+        weights = weightsList.ToArray();
+    }
+
+    //feedforward system using given input array
+    public float[] FeedForward(float[] inputs)
+    {
+        //add the inputs to the neuron matrix
+        for (int i = 0; i < inputs.Length; i++)
+        {
+            neurons[0][i] = inputs[i];
+        }
+        
+        //iteraate over all neurons and compute feedforward values
+        for (int i = 1; i < layers.Length; i++)
+        {
+            for (int j = 0; j < neurons[i].Length; j++)
+            {
+                float value = 0f;
+
+                for (int k = 0; k < neurons[i-1].Length; k++)
+                {
+                    value += weights[i-1][j][k] * neurons[i-1][k];
+                }
+
+                neurons[i][j] = (float)Math.Tanh(value); //hyperbolic tangent activation -- look into differences?
+            }
+        }
+
+        return neurons[neurons.Length-1]; //returns the last layer, output
+    }
+
+    //mutate the weights
+    public void Mutate()
+    {
+        for (int i = 0; i < weights.Length; i++)
+        {
+            for (int j = 0; j < weights[i].Length; j++)
+            {
+                for (int k = 0; k < weights[i][j].Length; k++)
+                {
+                    float weight = weights[i][j][k];
+
+                    //mutate weight value
+                    float randomNumber = UnityEngine.Random.Range(0f, 100f); //to get percentage chance out of 100
+
+                    if (randomNumber <= 1f) //1% chance of weight flipping polarity
+                    {
+                        weight *= -1f;
+                    }
+                    else if (randomNumber <= 2f) //1% chance of new random weight
+                    {
+                        weight = UnityEngine.Random.Range(-0.5f, 0.5f); 
+                    }
+                    else if (randomNumber <= 3f) //1% chance of increasing by random %
+                    {
+                        float factor = UnityEngine.Random.Range(0f, 1f) + 1f;
+                        weight *= factor;
+                    }
+                    else if (randomNumber <= 4f) //1% chance of decreasing by random %
+                    {
+                        float factor = UnityEngine.Random.Range(0f, 1f);
+                        weight *= factor;
+                    }
+
+                    weights[i][j][k] = weight;
+                }
+            }
+        }
+    }
+
+    //fitness functions
+    public void AddFitness(float fit)
+    {
+        fitness += fit;
+    }
+    public void SetFitness(float fit)
+    {
+        fitness = fit;
+    }
+    public float GetFitness()
+    {
+        return fitness;
+    }
+    public void NormalizeFitness(float sum)
+    {
+        normalizedFitness = fitness / sum;
+    }
+    public float GetNormalizedFitness()
+    {
+        return normalizedFitness;
+    }
+    // compare and sort NNs based on fitness -- ascending
+    public int CompareTo(NeuralNetwork other)
+    {
+        if (other == null) return 1;
+
+        if (fitness > other.fitness)
+        {
+            return 1;
+        }
+        else if (fitness < other.fitness)
+        {
+            return -1;
+        }
+        else
+        {
+            return 0;
+        }
+
+    }
+}
---
+++ unity/27_MarbleMadness/Assets/Scripts/SensorRays.cs
@@ -0,0 +1,175 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SensorRays : MonoBehaviour
+{
+    //based on EYEmaginary's "Car AI Tutorial" Series, thanks!!
+    //Sensors
+    //TODO: if time, make these mutable also?
+    public float fallSensorLength = 6f;
+    public Vector3 frontSensorPos = new Vector3 (0f, 0.2f, 0.5f);
+    public Vector3 rightSensorPos = new Vector3 (0.5f, 0.2f, 0f);
+    public Vector3 backSensorPos = new Vector3 (0f, 0.2f, -0.5f);
+    public Vector3 leftSensorPos = new Vector3 (-0.5f, 0.2f, 0f);
+
+    
+    public Vector3 frontRightSensorPos = new Vector3 (0.3f, 0.2f, 0.3f);
+    public Vector3 backRightSensorPos = new Vector3 (0.3f, 0.2f, -0.3f);
+    public Vector3 backLeftSensorPos = new Vector3 (-0.3f, 0.2f, -0.3f);
+    public Vector3 frontLeftSensorPos = new Vector3 (-0.3f, 0.2f, 0.3f);
+    // public float frontSideSensorPosition = 0.2f;
+    public float sideSensorAngle = 45f; //for the corner angles
+    public float fallSensorAngle = 20f; //towards the ground
+
+    // public float[] sensorsNormalized; // all the normalized values of distance to X, starting with goal and front and going clockwise
+    // public float[] nnInputs; //the same as sensorsNormalized
+    //ah, arrays can't be resized... copy needed? basically redundant...
+    
+    
+    //goal
+    // [SerializeField] GameObject goal;
+    // private Transform goalPos = goal.transform;
+    public Transform goal;
+    private float startGoalDistance;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startGoalDistance = Vector3.Distance(transform.position, goal.position);
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        CheckSensors();
+    }
+
+    private void CheckSensors()
+    {
+        //to avoid rotation issue, changing all transform.forward/up/right to Vector3.etc
+        //now issue is that it's always facing the z direction... should change based on input forward dir...
+        //trying to replace with inputForce now.
+        //nope, okay, nvm, just gonna surround the ball with sensors lol.
+        RaycastHit hit;
+        Vector3 sensorStartPos = transform.position;
+        // Vector3 sensorDirection = inputForce;
+
+        // sensorStartPos += Vector3.forward * frontSensorPos.z;
+        // sensorStartPos += Vector3.up * frontSensorPos.y;
+        // Debug.Log(sensorStartPos);
+
+        //reset sensor array
+        float[] sensorsNormalized = new float[9];
+
+        //ray to goal
+        float goalDistance = Vector3.Distance(transform.position, goal.position);
+        float normalizedGoalDistance = goalDistance / startGoalDistance;
+        Vector3 goalDirection = (goal.position - transform.position);
+        if(Physics.Raycast(sensorStartPos, goalDirection, out hit, 500f)){
+            Debug.DrawLine(sensorStartPos, hit.point);
+            // Debug.Log(normalizedGoalDistance);
+            sensorsNormalized[0] = normalizedGoalDistance;
+        } else {
+            Debug.Log("where the hell are you? Did you fall off?");
+        }
+
+        //front center fall sensor
+        Vector3 frontStartPos = transform.position;
+        frontStartPos += Vector3.forward * frontSensorPos.z;
+        frontStartPos += Vector3.up * frontSensorPos.y;
+        if (Physics.Raycast(frontStartPos, Quaternion.AngleAxis(fallSensorAngle, Vector3.right) * Vector3.forward, out hit, fallSensorLength)){
+            Debug.DrawLine(frontStartPos, hit.point);
+            sensorsNormalized[1] = hit.distance / fallSensorLength;
+        } else {
+            sensorsNormalized[1] = 1;
+        }
+        //front right fall sensor
+        Vector3 frontRightStartPos = transform.position;
+        frontRightStartPos += Vector3.forward * frontRightSensorPos.z;
+        frontRightStartPos += Vector3.up * frontRightSensorPos.y;
+        frontRightStartPos += Vector3.right * frontRightSensorPos.x;
+        if (Physics.Raycast(frontRightStartPos, Quaternion.AngleAxis(fallSensorAngle, Vector3.right) * Quaternion.AngleAxis(sideSensorAngle, Vector3.up) * Vector3.forward, out hit, fallSensorLength)){
+            Debug.DrawLine(frontRightStartPos, hit.point);
+            sensorsNormalized[2] = hit.distance / fallSensorLength;
+        } else {
+            sensorsNormalized[2] = 1;
+        }
+        //right fall sensor
+        Vector3 rightStartPos = transform.position;
+        rightStartPos += Vector3.up * rightSensorPos.y;
+        rightStartPos += Vector3.right * rightSensorPos.x;
+        if (Physics.Raycast(rightStartPos, Quaternion.AngleAxis(-fallSensorAngle, Vector3.forward) * Quaternion.AngleAxis(sideSensorAngle * 2f, Vector3.up) * Vector3.forward, out hit, fallSensorLength)){
+            Debug.DrawLine(rightStartPos, hit.point);
+            sensorsNormalized[3] = hit.distance / fallSensorLength;
+        }else {
+            sensorsNormalized[3] = 1;
+        }
+        //back right fall sensor
+        Vector3 backRightStartPos = transform.position;
+        backRightStartPos += Vector3.forward * backRightSensorPos.z;
+        backRightStartPos += Vector3.up * backRightSensorPos.y;
+        backRightStartPos += Vector3.right * backRightSensorPos.x;
+        if (Physics.Raycast(backRightStartPos, Quaternion.AngleAxis(-fallSensorAngle, Vector3.right) * Quaternion.AngleAxis(sideSensorAngle * 3f, Vector3.up) * Vector3.forward, out hit, fallSensorLength)){
+            Debug.DrawLine(backRightStartPos, hit.point);
+            sensorsNormalized[4] = hit.distance / fallSensorLength;
+        }else {
+            sensorsNormalized[4] = 1;
+        }
+        //back fall sensor
+        Vector3 backStartPos = transform.position;
+        backStartPos += Vector3.forward * backSensorPos.z;
+        backStartPos += Vector3.up * backSensorPos.y;
+        if (Physics.Raycast(backStartPos, Quaternion.AngleAxis(-fallSensorAngle, Vector3.right) * Vector3.back, out hit, fallSensorLength)){
+            Debug.DrawLine(backStartPos, hit.point);
+            sensorsNormalized[5] = hit.distance / fallSensorLength;
+        }else {
+            sensorsNormalized[5] = 1;
+        }
+        //back left fall sensor
+        Vector3 backLeftStartPos = transform.position;
+        backLeftStartPos += Vector3.forward * backLeftSensorPos.z;
+        backLeftStartPos += Vector3.up * backLeftSensorPos.y;
+        backLeftStartPos += Vector3.right * backLeftSensorPos.x;
+        if (Physics.Raycast(backLeftStartPos, Quaternion.AngleAxis(-fallSensorAngle, Vector3.right) * Quaternion.AngleAxis(-sideSensorAngle * 3f, Vector3.up) * Vector3.forward, out hit, fallSensorLength)){
+            Debug.DrawLine(backLeftStartPos, hit.point);
+            sensorsNormalized[6] = hit.distance / fallSensorLength;
+        }else {
+            sensorsNormalized[6] = 1;
+        }
+        //left fall sensor
+        Vector3 leftStartPos = transform.position;
+        leftStartPos += Vector3.up * leftSensorPos.y;
+        leftStartPos += Vector3.right * leftSensorPos.x;
+        if (Physics.Raycast(leftStartPos, Quaternion.AngleAxis(fallSensorAngle, Vector3.forward) * Quaternion.AngleAxis(-sideSensorAngle * 2f, Vector3.up) * Vector3.forward, out hit, fallSensorLength)){
+            Debug.DrawLine(leftStartPos, hit.point);
+            sensorsNormalized[7] = hit.distance / fallSensorLength;
+        }else {
+            sensorsNormalized[7] = 1;
+        }
+        //front left fall sensor
+        Vector3 frontLeftStartPos = transform.position;
+        frontLeftStartPos += Vector3.forward * frontLeftSensorPos.z;
+        frontLeftStartPos += Vector3.up * frontLeftSensorPos.y;
+        frontLeftStartPos += Vector3.right * frontLeftSensorPos.x;
+        if (Physics.Raycast(frontLeftStartPos, Quaternion.AngleAxis(fallSensorAngle, Vector3.right) * Quaternion.AngleAxis(-sideSensorAngle, Vector3.up) * Vector3.forward, out hit, fallSensorLength)){
+            Debug.DrawLine(frontLeftStartPos, hit.point);
+            sensorsNormalized[8] = hit.distance / fallSensorLength;
+        }else {
+            sensorsNormalized[8] = 1;
+        }
+
+        Debug.Log("0: " + sensorsNormalized[0]);
+        Debug.Log("1: " + sensorsNormalized[1]);
+        Debug.Log("2: " + sensorsNormalized[2]);
+        Debug.Log("3: " + sensorsNormalized[3]);
+        Debug.Log("4: " + sensorsNormalized[4]);
+        Debug.Log("5: " + sensorsNormalized[5]);
+        Debug.Log("6: " + sensorsNormalized[6]);
+        Debug.Log("7: " + sensorsNormalized[7]);
+        Debug.Log("8: " + sensorsNormalized[8]);
+
+
+    }
+}
